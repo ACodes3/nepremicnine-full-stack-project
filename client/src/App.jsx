b47d99ef6f8d27826d4316cd9bd0f4fbaf318d80
Aspasija Cvetkoska <aspasija.codes@gmail.com>
2024-05-18 09:15:05 +0200

    Responsive Hero Section Design

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,13 +1,18 @@
-import Navbar from "./components/Navbar"
-import "./layout.scss"
-
+import Navbar from "./components/Navbar";
+import "./layout.scss";
+import HomePage from "./pages/HomePage/HomePAge";
 
 function App() {
   return (
     <div className="layout">
-      <Navbar/>
+      <div className="navbar">
+        <Navbar />
+      </div>
+      <div className="content">
+        <HomePage />
+      </div>
     </div>
-  )
+  );
 }
 
-export default App
+export default App;
---
+++ client/src/pages/HomePage/HomePAge.jsx
@@ -0,0 +1,37 @@
+import SearchBar from "../../components/SearchBar/SearchBar";
+import "./homepage.scss";
+
+function HomePage() {
+  return (
+    <div className="homePage">
+      <div className="textContainer">
+        <div className="wrapper">
+          <h1 className="title">Find Real Estate & Get Your Dream Place</h1>
+          <p>
+            Lorem ipsum dolor sit amet consectetur adipisicing elit. Aliquid cupiditate veniam, libero quam voluptate voluptatem repudiandae laudantium quia animi commodi? Molestias asperiores laudantium mollitia sunt consectetur amet eos nam tempora?
+          </p>
+          <SearchBar/>
+          <div className="boxes">
+            <div className="box">
+                <h1>16+</h1>
+                <h2>Years of Experience</h2>
+            </div>
+            <div className="box">
+                <h1>200</h1>
+                <h2>Awards</h2>
+            </div>
+            <div className="box">
+                <h1>1200+</h1>
+                <h2>Properties</h2>
+            </div>
+          </div>
+        </div>
+      </div>
+      <div className="imgContainer">
+        <img src="/bg.png" alt="background-image" />
+      </div>
+    </div>
+  );
+}
+
+export default HomePage;
