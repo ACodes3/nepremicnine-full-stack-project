9ae6d03eaec87700dc9031c92f26239c71d59683
Aspasija Cvetkoska <aspasija.codes@gmail.com>
2024-05-18 15:00:26 +0200

    some additional pages and sub-pages added

--- client/src/pages/ListPage/ListPage.jsx
+++ client/src/pages/ListPage/ListPage.jsx
@@ -1,8 +1,20 @@
+import Filter from "../../components/Filter/Filter";
+import { listData } from "../../lib/dummydata";
 import "./listpage.scss";
 
 function ListPage(){
+
+    const data = listData;
+
     return(
-        <div className="listPage">ListPage</div>
+        <div className="listPage">
+            <div className="listContainer">
+                <div className="wrapper">
+                    <Filter/>
+                </div>
+            </div>
+            <div className="mapContainer">Map</div>
+        </div>
     )
 }
 
