6e8395088a30ce90611f6305b834e0b07e037f2e
Aspasija Cvetkoska <aspasija.codes@gmail.com>
2024-05-18 14:43:20 +0200

    added react router dom for navigation

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,17 +1,38 @@
-import Navbar from "./components/Navbar";
-import "./layout.scss";
 import HomePage from "./pages/HomePage/HomePAge";
 
+import {
+  createBrowserRouter,
+  RouterProvider,
+} from "react-router-dom";
+import ListPage from "./pages/ListPage/ListPage";
+import Layout from "./pages/Layout/Layout";
+
 function App() {
+
+  const router = createBrowserRouter([
+    {
+      path: "/",
+      element: 
+          <Layout/>,
+          children:[
+            {
+              path: "/",
+              element: <HomePage/>
+            },
+            {
+              path: "/list",
+              element: <ListPage/>
+            }
+          ]
+    },
+    {
+      path: "/list",
+      element: <ListPage/>,
+    },
+  ]);
+
   return (
-    <div className="layout">
-      <div className="navbar">
-        <Navbar />
-      </div>
-      <div className="content">
-        <HomePage />
-      </div>
-    </div>
+   <RouterProvider router={router} />
   );
 }
 
