ffb878d7ea0b1fb577b3a50ee9fbce1d86cc14a1
Aspasija Cvetkoska <aspasija.codes@gmail.com>
2024-06-17 20:40:31 +0200

    added database schema

--- server/utils/db.js
+++ server/utils/db.js
@@ -1,11 +1,12 @@
 import pkg from 'pg';
-const { Client } = pkg;
+const { Pool } = pkg;
 
-const con = new Client({
+const con = new Pool({
     host: 'localhost',
     user: 'postgres',
     password: 'postgres',
     database: 'a_real_estate_20240605',
+    port: 5432, // default port for PostgreSQL
 });
 
 con.connect(err => {
