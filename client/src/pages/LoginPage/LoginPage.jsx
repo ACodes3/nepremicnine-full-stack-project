f697389fe39b96aa4edb9975b1729d51e2953a89
Aspasija Cvetkoska <aspasija.codes@gmail.com>
2024-06-18 10:33:26 +0200

    admin login + redirect

--- client/src/pages/LoginPage/LoginPage.jsx
+++ client/src/pages/LoginPage/LoginPage.jsx
@@ -1,25 +1,60 @@
+import { useState } from 'react';
 import "./loginpage.scss";
-import { Link } from "react-router-dom";
+import { Link, useNavigate } from "react-router-dom";
+import axios from "axios";
 
 function LoginPage() {
+  const [ value, setValue] = useState({
+    admin_email: "",
+    admin_password: "",
+});
+
+const [error, setError] = useState(null);
+
+const navigate = useNavigate();
+axios.defaults.withCredentials = true;
+
+const handleSubmit = (event) => {
+    event.preventDefault();
+    if (value.admin_email.trim() === '' || value.admin_password.trim() === '') {
+        // If email or password is empty, do not proceed with login
+        return;
+    }
+    axios.post("http://localhost:3000/auth/sign-in", value)
+        .then(result => {
+            if(result.data.loginStatus) {
+                localStorage.setItem("valid", true)
+                navigate("/dashboard");
+            } else {
+                setError(result.data.Error)
+            }
+            
+        })
+        .catch(err => console.log(err));
+};
   return (
     <div className="loginPage">
       <div className="formContainer">
-        <form onSubmit={() => {}}>
+      <div className='text-warning'>
+                {error && error}
+            </div>
+        <form onSubmit={handleSubmit}>
           <h1>Welcome back</h1>
           <input
-            name="username"
+            name="email"
             required
             minLength={3}
-            maxLength={20}
-            type="text"
-            placeholder="Username"
+            maxLength={50}
+            type="email"
+            placeholder="Email"
+            onChange={(e) => setValue({...value, admin_email: e.target.value})}
           />
           <input
             name="password"
             type="password"
             required
             placeholder="Password"
+            onChange={(e) => setValue({...value, admin_password: e.target.value})}
           />
           <button>Login</button>
           <Link to="/sign-up">{"Don't"} you have an account?</Link>
--- server/Routes/AdminRoute.js
+++ server/Routes/AdminRoute.js
@@ -7,6 +7,20 @@ import path from "path";
 
 const router = express.Router()
 
+//LOGIN ROUTE
+router.post("/sign-in", (req, res) => {
+    const sql = "SELECT * from admin Where admin_email = ? and admin_password = ?"
+    con.query(sql, [req.body.admin_email, req.body.admin_password], (err, result) => {
+        if (err) return res.json({ loginStatus: false, Error: "Query error" })
+        if (result.length > 0) {
+            const email = result[0].admin_email;
+            const token = jwt.sign({ role: "admin", email: email }, "jwt_secret_key", { expiresIn: "1d" })
+            res.cookie("token", token)
+            return res.json({ loginStatus: true })
+        } else return res.json({ loginStatus: false, Error: "Wrong email or password" })
+    })
+})
+
 
 
 
